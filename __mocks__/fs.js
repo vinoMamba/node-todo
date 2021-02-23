4e6d6d04d502a5dae15cd850cf4744681921bdaa
vino <vino0908@outlook.com>
2021-02-23 23:41:49 +0800

    添加 db.write的单元测试

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -3,19 +3,32 @@ const _fs = jest.requireActual('fs')
 
 Object.assign(fs, _fs)
 
-const mocks = {}
-fs.setMock = (path, error, data) => {
-    mocks[path] = [error, data]
+const readMocks = {}
+fs.setReadMock = (path, error, data) => {
+    readMocks[path] = [error, data]
 }
 
 fs.readFile = (path, options, callback) => {
     if (callback === undefined) {
         callback = options
     }
-    if (path in mocks) {
-        callback(...mocks[path])
+    if (path in readMocks) {
+        callback(...readMocks[path])
     } else {
         _fs.readFile(path, options, callback)
     }
 }
+
+const writeMocks = {}
+fs.setWriteMock = (path, fn) => {
+    writeMocks[path] = fn
+}
+fs.writeFile = (path, data, options, callback) => {
+    if (path in writeMocks) {
+        writeMocks[path](path, data, options, callback)
+    } else {
+        _fs.writeFile(path, data, options, callback)
+    }
+}
+
 module.exports = fs
--- __tests__/db.spec.js
+++ __tests__/db.spec.js
@@ -6,8 +6,18 @@ jest.mock('fs')
 describe('db', () => {
     it('can read', async () => {
         const data = [{taskName: 'hi', done: true}]
-        fs.setMock('/xxx', null, JSON.stringify(data))
+        fs.setReadMock('/xxx', null, JSON.stringify(data))
         const list = await db.read('/xxx')
         expect(list).toStrictEqual(data)
+    });
+    it('can write', async () => {
+        let fakeFile
+        fs.setWriteMock('/yyy', (path, data, callback) => {
+            fakeFile = data
+            callback(null)
+        })
+        const list = [{taskName: 'hi', done: true}]
+        await db.write(list, '/yyy')
+        expect(fakeFile).toBe(JSON.stringify(list))
     });
 })
